9ae4ab66889172220cc90bbaebf0cfbfbbfd1f94
William Joseph Mark <wjosephmark@gmail.com>
2022-04-21 13:03:15 -0600

    formatted resolution statement to encompass all types of queries

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const { Client } = require("pg");
 const queryCentric = async (query) => {
   var response = {};
 
-  const client = new Client({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    port: process.env.PORT,
-  });
+  var clientOptions = {
+    host: process.env.CENTRIC_HOST,
+    user: process.env.CENTRIC_USER,
+    password: process.env.CENTRIC_PASSWORD,
+    database: process.env.CENTRIC_DATABASE,
+    port: process.env.CENTRIC_PORT,
+  };
+
+  const client = new Client(clientOptions);
 
   client.connect();
 
@@ -22,16 +24,22 @@ const queryCentric = async (query) => {
         client.end();
         reject("Error pushing data to aurora");
       } else {
-        console.log(res);
+        response = res;
         client.end();
-        resolve("Data pushed to aurora successfully");
+        resolve("Query passed to aurora successfully");
       }
     });
   });
+
+  return response;
 };
 
 const handler = async () => {
-  queryCentric();
+  var query = "SELECT * FROM staging.staging_ariba_testing";
+
+  var centricResponse = await queryCentric(query);
+
+  console.log(centricResponse);
 };
 
 handler();
